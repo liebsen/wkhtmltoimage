b138b07b87d771ec0d03546d7590029f89d67ad8
liebsen <overlemonsoft@gmail.com>
2022-05-15 15:23:59 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require("path")
+const wkhtmltoimage = require('wkhtmltoimage')
+const uploads_folder = 'captures'
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/views/index.html`))
+})
+
+app.post('/shot', (req, res) => {
+  var url = req.body.url
+  var success = false
+  if (url) {
+    var filename = url.replace(/[^a-z0-9]/gi, '_').toLowerCase()
+    wkhtmltoimage.generate(url, { output: path.join(`${__dirname}/${uploads_folder}/${filename}.jpg`) })
+    success = true
+  }
+  res.json({ success: success })
+})
