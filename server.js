4f62a866a256b688ae3b04cbb06710c9961ceb10
liebsen <overlemonsoft@gmail.com>
2022-05-15 16:30:44 -0300

    build

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const path = require("path")
 const wkhtmltoimage = require('wkhtmltoimage')
 const uploads_folder = 'captures'
 const port = 5555
+app.use(express.json())
+
+wkhtmltoimage.generate('https://google.com/', { output: 'out.jpg' });
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/views/index.html`))
 })
 
-app.post('/shot', (req, res) => {
+app.post('/capture', (req, res) => {
+  console.log(req.body)
   var url = req.body.url
   var success = false
   if (url) {
     var filename = url.replace(/[^a-z0-9]/gi, '_').toLowerCase()
-    wkhtmltoimage.generate(url, { output: path.join(`${__dirname}/${uploads_folder}/${filename}.jpg`) })
+    let filepath = path.join(`${__dirname}/${uploads_folder}/${filename}.jpg`)
+    console.log(filename)
+    console.log(filepath)
+    wkhtmltoimage.generate(url, { output: filepath })
     success = true
   }
   res.json({ success: success })
