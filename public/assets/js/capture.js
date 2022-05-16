7d7c35450b1870b3f9654482d4e3e54d542ad296
liebsen <overlemonsoft@gmail.com>
2022-05-16 20:44:29 -0300

    build

--- public/assets/js/capture.js
+++ public/assets/js/capture.js
@@ -1,8 +1,25 @@
 
+function isValidHttpUrl(string) {
+  let url;
+  
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
 function capture () {
+  let url = document.getElementById('capture_url').value
+  if (!isValidHttpUrl(url)) {
+    alert('Please enter a valid URL.')
+    return document.getElementById('capture_url').focus()
+  }
   const button = document.querySelector('.send-btn')
   let data = {
-    url: document.getElementById('capture_url').value
+    url: url
   }
   button.classList.add('is-loading')
   var request = new Request('/capture', {
