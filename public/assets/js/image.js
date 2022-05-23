332ba675ded7c8260ecb7cdb2419bd282e23f975
liebsen <overlemonsoft@gmail.com>
2022-05-23 17:39:40 -0300

    build

--- public/assets/js/image.js
+++ public/assets/js/image.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   var img = document.getElementById('img_loader');
   console.log("The image is loading...")
-
   img.onload = () => {
     document.querySelector('.capture-container').classList.remove('is-loading')
     document.querySelector('.capture-img').classList.remove('skewInDesktop')
@@ -12,4 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.capture-img').style.backgroundImage = `url(${img.src})`
     console.log("The image has loaded!")
   }
+  setTimeout(function(){
+    img.src = img.src
+  }, 500)    
 })
